d4ec77ec302898a7f448b728025670e4c4ab5ec3
Ruize Nie <rz376775@dal.ca>
2020-07-27 16:39:21 -0300

    style the skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,50 +9,59 @@ const Skills = () => {
         <hr />
       </header>
       <div className="skills-wrapper">
-        <div class="qual full-qual">
-          <div class="data">
-            <div class="up-arrow data-item" id="item1">
-              <h5>Awesome Place</h5>
-              <span>&nbsp;</span>
-            </div>
-
-            <div class="data-item" id="item2">
-              <h5>Awesome Creation</h5>
-              <span>&nbsp;</span>
-            </div>
-
-            <div class="data-item" id="item3">
-              <h5>Cool Place</h5>
-              <span>&nbsp;</span>
-            </div>
-
-            <div class="data-item" id="item4">
-              <h5>Cool Place</h5>
-              <span>&nbsp;</span>
-            </div>
-
-            <div class="data-item" id="item5">
-              <h5>Cool Place</h5>
-              <span>&nbsp;</span>
-            </div>
-
-            <div class="data-item" id="item6">
-              <h5>Cool Place</h5>
-              <span>&nbsp;</span>
-            </div>
-
-            <div class="data-item" id="item7">
-              <h5>
-                Major Cool
-                <br />
-                Place
-              </h5>
-              <span>&nbsp;</span>
-            </div>
+        <div className="data">
+          <div className="data-item" id="item1">
+            <h5>JavaScript</h5>
+            <span>&nbsp;</span>
           </div>
 
-          <hr />
+          <div className="data-item" id="item8">
+            <h5>Java</h5>
+            <span>&nbsp;</span>
+          </div>
+
+          <div className="data-item" id="item2">
+            <h5>AWS Cloud Server</h5>
+            <span>&nbsp;</span>
+          </div>
+
+          <div className="data-item" id="item9">
+            <h5>Docker</h5>
+            <span>&nbsp;</span>
+          </div>
+
+          <div className="data-item" id="item3">
+            <h5>Database</h5>
+            <span>&nbsp;</span>
+          </div>
+
+          <div className="data-item" id="item4">
+            <h5>Python</h5>
+            <span>&nbsp;</span>
+          </div>
+
+          <div className="data-item" id="item5">
+            <h5>Node</h5>
+            <span>&nbsp;</span>
+          </div>
+
+          <div className="data-item" id="item6">
+            <h5>React & React Native</h5>
+            <span>&nbsp;</span>
+          </div>
+
+          <div className="data-item" id="item7">
+            <h5>HTML & CSS</h5>
+            <span>&nbsp;</span>
+          </div>
         </div>
+        <hr />
+        <ul className="skills-field">
+          <li>DATA PROCESS</li>
+          <li>WEB DEVELOPMENT</li>
+          <li>MOBILE DEVELOPMENT</li>
+          <li>CLOUD SERVERLESS</li>
+        </ul>
       </div>
     </div>
   );
