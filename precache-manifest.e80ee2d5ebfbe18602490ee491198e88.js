f5651abfed91c669f56d9c5807f0234947600e8f
Ruize Nie <rz376775@dal.ca>
2020-07-27 22:55:23 -0300

    Updates

---
+++ precache-manifest.e80ee2d5ebfbe18602490ee491198e88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76e25c3b6717d6bf9b4df628bc89a27b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7c9699df59c3434bd66a",
+    "url": "/portfolio/static/css/main.10d9d7f3.chunk.css"
+  },
+  {
+    "revision": "911f985599e0d32ecf75",
+    "url": "/portfolio/static/js/2.5d635a66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.5d635a66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c9699df59c3434bd66a",
+    "url": "/portfolio/static/js/main.03cbb5b5.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "dfbf508f269cc291e25fe2be188b046e",
+    "url": "/portfolio/static/media/avatar.dfbf508f.jpg"
+  }
+]);
